035c012b3b8b4291de3cc5942dd4bc449fc209a5
maleiml <malei@laiye.com>
2020-03-17 14:09:46 +0800

    样式修改

--- src/components/MsgContent/VideoContent.tsx
+++ src/components/MsgContent/VideoContent.tsx
@@ -15,6 +15,12 @@ export default function VideoContent({ body }: IProps) {
       <video preload="true" src={resource_url} className={styles.video}>
         您的浏览器不支持video标签
       </video>
+
+      <div className={styles.model}>
+        <div className={styles.playBox}>
+          <img src="https://laiye-im-saas.oss-cn-beijing.aliyuncs.com/8b03e713-d24c-406e-98d9-7e60ea2090af.png"/>
+        </div>
+      </div>
     </div>
   )
 }
