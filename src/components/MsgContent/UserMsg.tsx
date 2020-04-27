74570a0de86b0e9df830c1db57b47e7cd4a22a0b
maleiml <malei@laiye.com>
2020-04-27 18:30:09 +0800

    改成function组件

--- src/components/MsgContent/UserMsg.tsx
+++ src/components/MsgContent/UserMsg.tsx
@@ -11,44 +11,40 @@ interface IProps {
   message: IMsgBodyInfo
 }
 
-class UserMsg extends Nerv.Component<IProps> {
-  props: IProps
+const UserMsg = (props: IProps) => {
+  const { message } = props
+  const bgColor = PageConfig.get('theme_color') as string
+  const avatarShape = AVATAR_SHAPE[PageConfig.get('avatar_shape')]
+  const chatBar = CHAT_BAR[PageConfig.get('chat_bar')]
+  const avatar = PageConfig.get('user_avatar')
 
-  render() {
-    const { message } = this.props
-    const bgColor = PageConfig.get('theme_color') as string
-    const avatarShape = AVATAR_SHAPE[PageConfig.get('avatar_shape')]
-    const chatBar = CHAT_BAR[PageConfig.get('chat_bar')]
-    const avatar = PageConfig.get('user_avatar')
+  const hasOwnContent =
+    message.msg_type === 'IMAGE' ||
+    message.msg_type === 'FILE' ||
+    message.msg_type === 'SHARELINK' ||
+    message.msg_type === 'VIDEO'
 
-    const hasOwnContent =
-      message.msg_type === 'IMAGE' ||
-      message.msg_type === 'FILE' ||
-      message.msg_type === 'SHARELINK' ||
-      message.msg_type === 'VIDEO'
+  const cls = !hasOwnContent ? styles.content : ''
 
-    const cls = !hasOwnContent ? styles.content : ''
+  return(
+    <div className={`${styles.msgContent} ${styles.clearfix}`}>
+      <dl className={`${styles.msgContainer} ${styles.userMsg}`}>
+        <dt className={styles.userInfo}>
+          <div className={`${styles.avatar} ${avatarShape}`} style={{ backgroundColor: bgColor }}>
+            <img src={`${avatar}?x-oss-process=image/resize,w_72,h_72/quality,q_80`}/>
+          </div>
+        </dt>
 
-    return(
-      <div className={`${styles.msgContent} ${styles.clearfix}`}>
-        <dl className={`${styles.msgContainer} ${styles.userMsg}`}>
-          <dt className={styles.userInfo}>
-            <div className={`${styles.avatar} ${avatarShape}`} style={{ backgroundColor: bgColor }}>
-              <img src={`${avatar}?x-oss-process=image/resize,w_72,h_72/quality,q_80`}/>
-            </div>
-          </dt>
+        <dd className={styles.msgBody}>
+          <div className={`${cls} ${chatBar}`}>
+            <MsgContent message={message} />
+          </div>
 
-          <dd className={styles.msgBody}>
-            <div className={`${cls} ${chatBar}`}>
-              <MsgContent message={message} />
-            </div>
-
-            <span className={styles.hiddenId}>#{message.msg_id}</span>
-          </dd>
-        </dl>
-      </div>
-    )
-  }
+          <span className={styles.hiddenId}>#{message.msg_id}</span>
+        </dd>
+      </dl>
+    </div>
+  )
 }
 
 export default UserMsg as any
