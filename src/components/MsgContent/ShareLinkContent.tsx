fe8928365afe9f2dbe1197402f52fb3a76988f69
maleiml <malei@laiye.com>
2020-05-28 15:34:23 +0800

    --bug=1011071 --user=马蕾 【测试环境】websdk-体... https://www.tapd.cn/20953051/s/1145806

--- src/components/MsgContent/ShareLinkContent.tsx
+++ src/components/MsgContent/ShareLinkContent.tsx
@@ -8,10 +8,10 @@ interface IProps {
 }
 
 export default function VideoContent({ body }: IProps) {
-  const { title, description, cover_url } = body.share_link
+  const { title, description, cover_url, destination_url } = body.share_link
 
   return(
-    <a className={styles.shareLinkContent}>
+    <a className={styles.shareLinkContent} href={destination_url} target="_blank">
       <dl>
         <dt><h5>{title}</h5></dt>
         <dd>
