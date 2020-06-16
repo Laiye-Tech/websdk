8c7e7f236eb7bb7b2eb4da6ac54d089e5ca4b821
maleiml <malei@laiye.com>
2020-06-16 17:45:15 +0800

    fixed

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,19 +7,9 @@ let pageConfig: IPageConfig
 let envLanguage: any
 let interaction: IInteractionConfig
 const pkg = require('../../package.json')
-// const isProd = process.env.NODE_ENV === 'production'
-// const MODE = window.__WEB_SDK_CONF__ && window.__WEB_SDK_CONF__.ENV.MODE
-const BASE_URL = 'https://newtestcb2.wul.ai'
-
-// if (MODE === 'pre') {
-//   BASE_URL = 'https://precb2.wul.ai'
-// } else if (MODE === 'dev' || !isProd) {
-//   BASE_URL = 'https://newtestcb2.wul.ai'
-// } else if (MODE === 'test') {
-//   BASE_URL = 'https://newtestcb2.wul.ai'
-// } else {
-//   BASE_URL = 'https://cb2.wul.ai'
-// }
+// const BASE_URL = 'https://newtestcb2.wul.ai'
+const BASE_URL = 'https://precb2.wul.ai'
+// const BASE_URL = 'https://cb2.wul.ai'
 
 const page = {
   set: (configOb: IPageConfig): void => {
