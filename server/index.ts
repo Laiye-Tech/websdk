f70040ea47ccb96538d9211b65f1ddb259bd22d1
徐丽婧 <xulijing@laiye.com>
2020-08-07 19:29:26 +0800

    fix style

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ const html = `
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <meta name="viewport" content="width=device-width,initial-scale=1, user-scalable=no">
     <title>网页机器人</title>
   </head>
   <body style="background: #eceef0">
@@ -35,20 +35,22 @@ const html = `
   </html>
 `
 
-http.createServer((req, res) => {
-  const obj = url.parse(req.url)
+http
+  .createServer((req, res) => {
+    const obj = url.parse(req.url)
 
-  if (/\.(js|ico|css|png)$/.test(obj.pathname)) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    if (/\.(js|ico|css|png)$/.test(obj.pathname)) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
 
-    try {
-      const file = fs.readFileSync(__dirname + obj.pathname)
-      res.end(file)
-    } catch (e) {
-      res.end(null)
+      try {
+        const file = fs.readFileSync(__dirname + obj.pathname)
+        res.end(file)
+      } catch (e) {
+        res.end(null)
+      }
     }
-  }
 
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.end(html)
-}).listen(8082)
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(html)
+  })
+  .listen(8082)
